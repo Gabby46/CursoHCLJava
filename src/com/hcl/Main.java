b918bf1b7e5d19dcc78ca9314eae1022addf94a3
Eduardo Herrera <eherrerad94@gmail.com>
2017-08-24 16:15:29 -0500

    initial commit

---
+++ src/com/hcl/Main.java
@@ -0,0 +1,13 @@
+package com.hcl;
+
+public class Main {
+
+	String name ="Gabby";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Main m = new Main();
+		System.out.print(m.name +" Hello World" );
+	}
+
+}
